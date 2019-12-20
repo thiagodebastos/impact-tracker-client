5b970f623d212bd84310285ad029b2916bf23498
Thiago de Bastos <thiago@thiagodebastos.com>
2019-12-20 11:03:25 +1100

    feature(avatar): add some accessible navigation

--- src/components/avatar/avatar-item/main.tsx
+++ src/components/avatar/avatar-item/main.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Avatar } from '../';
 
 interface Props {
     avatar: React.ReactNode;
@@ -8,11 +7,28 @@ interface Props {
     href?: string;
     isHover?: boolean;
     onClick: () => void;
+    isActive?: boolean;
 }
 
-export function AvatarItem({ primaryText, secondaryText, href, onClick, avatar }: Props) {
+export function AvatarItem({ primaryText, secondaryText, onClick, avatar, isActive }: Props) {
+    function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+        console.log(event);
+        onClick();
+    }
+
+    function handleKeyPress(event: React.KeyboardEvent<HTMLDivElement>) {
+        console.log(event);
+        onClick();
+    }
+
     return (
-        <div className="flex px-4 py-2 cursor-pointer hover:bg-gray-200" onClick={onClick}>
+        <div
+            className="flex px-4 py-2 cursor-pointer focus:bg-gray-200 hover:bg-gray-200"
+            onClick={handleClick}
+            onKeyPress={handleKeyPress}
+            role="button"
+            tabIndex={0}
+        >
             {React.isValidElement(avatar) ? React.cloneElement(avatar) : null}
             <div className="flex flex-col justify-center pl-2 ">
                 <div className="">{primaryText}</div>
